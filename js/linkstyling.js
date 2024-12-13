cf259e308a2e5c42aa562975f87ee307cda53151
maxsheridan <max@maxsheridan.com>
2024-12-13 13:23:33 +0200

    Init repo

---
+++ js/linkstyling.js
@@ -0,0 +1,1 @@
+var currentPath=window.location.pathname;var navLinks=document.querySelectorAll(".bunch-of-buttons a");navLinks.forEach(function(t){if(t.getAttribute("href")===currentPath){t.classList.add("active")}})
